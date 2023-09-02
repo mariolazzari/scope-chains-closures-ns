a1c8ee33c9f6268ddc66451787f66433315db6e9
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-02 22:20:18 +0200

    Done

--- scope.js
+++ scope.js
@@ -1,7 +1,11 @@
-function foo(){
-    var bar;
+function foo() {
+  var bar = true;
+  quux = 1;
 
-    function zip(){
-        var quux;
-    }
+  function zip() {
+    var quux = 2;
+    bar = true;
+  }
+
+  return zip;
 }
